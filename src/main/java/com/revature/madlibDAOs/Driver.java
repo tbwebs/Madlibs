4321e4f4d2e36204a44470df2fd05834f346d8b2
tbwebs <tbwebs14@gmail.com>
2022-04-22 12:03:04 -0700

    started driver code

--- src/main/java/com/revature/madlibDAOs/Driver.java
+++ src/main/java/com/revature/madlibDAOs/Driver.java
@@ -1,7 +1,55 @@
 package com.revature.madlibDAOs;
 
+import java.util.Scanner;
+
 public class Driver {
 	
+	private static UserModel createUser(String username, String password) {
+		
+		UserModel user = new UserModel(username, password);
+		
+		return user;
+	}
 	
-	
+	public static void main(String[] args) {
+		
+		//DAO instance
+		UserDAO userDAO = new UserDAO();
+		WordsDAO wordsDAO = new WordsDAO();
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Welcome please enter your selection:");
+		System.out.println("1 : log in"
+				+ "2 : register");
+		
+		int input = sc.nextInt();
+		
+		
+		if (input == 1) {
+			
+			System.out.println("Username: ");
+			String username = sc.next();
+			System.out.println("Password: ");
+			String password = sc.next();
+			
+			//Words model will go
+			
+			
+		} else {
+			
+			System.out.println("Create your username : ");
+			String username = sc.next();
+			System.out.println("Create your password : ");
+			String password = sc.next();
+			
+			UserModel currentUser = createUser(username, password);
+			
+			userDAO.addUser(currentUser);
+			
+		}
+		
+		
+		
+	}
 }
